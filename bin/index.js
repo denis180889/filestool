7be291c94ec04b5e07da2f1dd3175e07b0a50094
denis180889 <denis.pob@gmail.com>
2020-01-18 17:42:04 +0200

    Final version

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,2 @@
 #!/usr/bin/env node
-const filestool = require('../lib/filestool');
-
-(async () => {
-    try {
-        await filestool.process();
-    }
-    catch (err) {
-        //console.log(err.message)
-        throw err;
-        process.exit();
-    }
-})()
+require('../lib/filestool').main();
